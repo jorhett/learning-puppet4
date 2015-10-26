f97e5d1b80cde05520d214708ad351270e0d34bd
Jo Rhett <jrhett@netconsonance.com>
2015-10-25 17:20:35 -0700

    With the latest versions of puppet-agent the binaries are added to the path, so the symbolic links are no longer necessary.

--- bin/fastsetup.sh
+++ bin/fastsetup.sh
@@ -7,10 +7,6 @@ echo "Installing Puppet"
 sudo yum install -q -y http://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
 sudo yum install -q -y puppet-agent
 echo ""
-echo "Enabling convenience symlinks."
-sudo ln -s /opt/puppetlabs/bin/puppet /usr/bin/puppet
-sudo ln -s /opt/puppetlabs/bin/facter /usr/bin/facter
-sudo ln -s /opt/puppetlabs/bin/mco /usr/bin/mco
 echo "Changing owner of /etc/puppetlabs to vagrant user"
 sudo chown -R vagrant:vagrant /etc/puppetlabs
 exit 0
