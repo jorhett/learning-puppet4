f9d0eb4ef191a2fc050409ad5b2f19b3c9ed9c0e
Jo Rhett <jrhett@netconsonance.com>
2016-01-03 11:10:47 +0000

    Revised hierasample.pp to match the book.

--- manifests/hierasample.pp
+++ manifests/hierasample.pp
@@ -1,9 +1,13 @@
 # Always set a default value
-$running = hiera('puppet_running','running')
-$startatboot = hiera('puppet_startatboot','true')
+$status  = lookup({ name => 'puppet::status',  default_value => 'running' })
+$enabled = lookup({ name => 'puppet::enabled', default_value => true })
+
+notify { 'puppet-settings': 
+  message => "Status should be ${status}, start at boot ${enabled}.",
+}
 
 # Now the same code can be used regardless of the value
 service { 'puppet':
-  ensure => $running,
-  enable => $startatboot,
+  ensure => $status,
+  enable => $enabled,
 }
