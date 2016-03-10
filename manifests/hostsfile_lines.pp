78602c48e95c138566ea5b9a410d943954fe0988
Jo Rhett <jrhett@netconsonance.com>
2016-03-10 07:01:01 +0000

    Remove the need for stdlib

--- manifests/hostsfile_lines.pp
+++ manifests/hostsfile_lines.pp
@@ -1,7 +1,6 @@
 $hosts_line = '192.168.250.6 puppetserver.example.com puppet.example.com puppetserver'
 $host = $hosts_line.split(' ')
 with( *$host ) |$ipaddr, $hostname, *$aliases| {
-  $alias_display = join($aliases,',')
-  notice( "Host ${hostname} has IP ${ipaddr} and aliases ${alias_display}" )
+  notice( "Host ${hostname} has IP ${ipaddr} and aliases ${aliases}" )
 }
 
